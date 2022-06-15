8e0f71dd3f7b0b3845af3b09cc22354c82cd5192
FarooqAkram <farooqakram552@gmail.com>
2022-06-15 23:29:47 +0800

    Tsting second update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
-	export let name;
+	let firstName = "Farooq";
+	let lastName = "Akram";
+	$: name = firstName + " " + lastName;
+	let color = '#495a7a';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1 style="color: {color};">Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
